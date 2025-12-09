5dd72aa26121446800bc0e51490bfd426c9c56b7
rory12392 <rory9256@gmail.com>
2025-12-09 22:58:40 +0800

    add w12 code_4

---
+++ src/app/demo/shop_49/supa_49/layout.js
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono } from 'next/font/google';
+import NavbarShopNode_49 from '../_componemts/NavbarShopNode_49';
+
+//import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata = {
+  title: 'midterm p2_49',
+  description: '1141 Midterm P2',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <NavbarShopNode_49 />
+        {children}
+      </body>
+    </html>
+  );
+}
